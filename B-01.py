df811744bd7c249ae7c242bc07d0742bc804a86c
Snowy <joanna.liao@kingsway.school.nz>
2025-11-14 09:58:45 +1300

    Updated description on the codea

--- B-01.py
+++ B-01.py
@@ -3,7 +3,8 @@
 #Date: 29-10-2025
 #Version 2.0
 
-#Number checker
+#Number checker that checks whether the value inserted is more than 0
+#If the number is less than 0, it would print error
 def num_check(question):
     error= f"Please eneter a value that is more the 0 \n"
     while True:
@@ -17,20 +18,25 @@ def num_check(question):
        (print(error))
     return response
 
+#Welcome code to make it more human
 name=input("Welcome to my fence cost calculator, what is your name? ")
 
+#loop code so the program can go on and on if the user desires
+#If they don't want to continue they press any other key to quit
 keep_going= ""
 while keep_going == "":
-  #Get width 
+  #These variables get the value of the width, length, and cost
   width= (num_check(f"{name}, please enter your shape's width in metres "))
   length= (num_check(f"{name}, please enter your shapes's length in metres "))
   cost= (num_check(f"{name}, please enter the cost of the shape per metres "))
 
-  #calculate cost
+  #calculates the cost of the fence 
   perimeter = 2 * (width + length)
   price = perimeter * cost
 
-  #output price
+  #outputs the price for the user to see
   print(f"{name}, your cost for a fence with a perimeter of {perimeter}m is ${price}")
   keep_going=(input("Please press enter if you wish to continue or any key to quit"))
+
+  
 
